0223d62a0765943770c27db000c9b8b654f4d03d
rjdp <rjdp9736@gmail.com>
2015-06-24 18:43:10 +0530

    add docstrings to testcases

--- tests/Plugin_test.py
+++ tests/Plugin_test.py
@@ -5,28 +5,37 @@ from ..plugin import monit
 class PluginTestCase(unittest.TestCase):
 
     def test_monit_install(self):
+        """test _monit_install() functionality"""
         pass
 
     def test_monit_remove(self):
+        """test _monit_remove() functionality"""
         pass
 
     def test_monit_purge(self):
+        """test _monit_purge() functionality"""
         pass
 
     def test_monit_status(self):
+        """test _monit_status() functionality"""
         pass
 
     def test_monit_start(self):
+        """test _monit_start() functionality"""
         pass
 
     def test_monit_stop(self):
+        """test _monit_stop() functionality"""
         pass
 
     def test_monit_reload(self):
+        """test _monit_reload() functionality"""
         pass
 
     def test_monit_restart(self):
+        """test _monit_restart() functionality"""
         pass
 
     def test_monit_upgrade(self):
+        """test _monit_upgrade() functionality"""
         pass
