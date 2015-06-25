5fb2725180db81f5ea67867c2acd8c808417cf7e
rjdp <rjdp9736@gmail.com>
2015-06-25 14:23:29 +0530

    passing app obj. to internal methods

--- src/plugin/sample_plugin.py
+++ src/plugin/sample_plugin.py
@@ -11,7 +11,7 @@ a) We dont append additonal subcommands to whats already available under
                             ,reload,restart and upgrade)
 b) For each argument that we add , we also define a method to mimic
    each subcommand in "ee stack" w.r.t the argument eg)for --sample
-   we defined _sample_install(),_sample_remove() and so on .
+   we defined _sample_install(ee),_sample_remove(ee) and so on .
 c) For each subcommand under "ee stack" we define a hook-function which
    which is registered with their respective predefined hooks
 
@@ -26,47 +26,47 @@ from cement.core.controller import CementBaseController, expose
 
 # define Plugin's all of theCore methods
 
-def _sample_install():
+def _sample_install(ee):
     """All code for sample Installation goes here"""
     print("installing sample")
 
 
-def _sample_remove():
+def _sample_remove(ee):
     """All code for sample Removal goes here"""
     print("removing sample")
 
 
-def _sample_purge():
+def _sample_purge(ee):
     """All code for Purging sample goes here"""
     print("Purging sample")
 
 
-def _sample_status():
+def _sample_status(ee):
     """All code for showing sample status goes here"""
     print("shows sample status")
 
 
-def _sample_start():
+def _sample_start(ee):
     """All code for starting sample goes here"""
     print("starts sample")
 
 
-def _sample_stop():
+def _sample_stop(ee):
     """All code for stopping sample goes here"""
     print("stops sample")
 
 
-def _sample_reload():
+def _sample_reload(ee):
     """All code for reloading sample goes here"""
     print("reloads sample")
 
 
-def _sample_restart():
+def _sample_restart(ee):
     """All code for restarting sample goes here"""
     print("restarts sample")
 
 
-def _sample_upgrade():
+def _sample_upgrade(ee):
     """All code for sample upgradation goes here"""
     print("upgrades sample")
 
@@ -80,14 +80,14 @@ def sample_install_hookfunc(ee):
         """All code for this method lives in this block w.r.t our use-case unless
         u know what ur doing, this code-block is executed when
         `ee stack install --sample` is issued"""
-        _sample_install()
+        _sample_install(ee)
 
     # Additional if-blocks like the above if-block should be added in case you are 
     # - adding more than one argument using a single plugin
     # eg) we add --goaccess argument
             """
             if ee.pargs.goaccess:
-                _goaccess_install()
+                _goaccess_install(ee)
             """
 
 
@@ -97,14 +97,14 @@ def sample_remove_hookfunc(ee):
         """All code for this method lives in this block w.r.t our use-case unless
         u know what ur doing, this code-block is executed when
         `ee stack remove --sample` is issued"""
-        _sample_remove()
+        _sample_remove(ee)
 
     # Additional if-blocks like the above if-block should be added in case you are 
     # - adding more than one argument using a single plugin
     # eg) we add --goaccess argument
             """
             if ee.pargs.goaccess:
-                _goaccess_remove()
+                _goaccess_remove(ee)
             """
 
 
@@ -114,14 +114,14 @@ def sample_purge_hookfunc(ee):
         """All code for this method lives in this block w.r.t our use-case unless
         u know what ur doing, this code-block is executed when
         `ee stack purge --sample` is issued"""
-        _sample_purge()
+        _sample_purge(ee)
 
     # Additional if-blocks like the above if-block should be added in case you are
     # - adding more than one argument using a single plugin
     # eg) we add --goaccess argument
             """
             if ee.pargs.goaccess:
-                _goaccess_purge()
+                _goaccess_purge(ee)
             """
 
 
@@ -131,14 +131,14 @@ def sample_status_hookfunc(ee):
         """All code for this method lives in this block w.r.t our use-case unless
         u know what ur doing, this code-block is executed when
         `ee stack status --sample` is issued"""
-        _sample_status()
+        _sample_status(ee)
 
     # Additional if-blocks like the above if-block should be added in case you are
     # - adding more than one argument using a single plugin
     # eg) we add --goaccess argument
             """
             if ee.pargs.goaccess:
-                _goaccess_status()
+                _goaccess_status(ee)
             """
 
 
@@ -148,14 +148,14 @@ def sample_start_hookfunc(ee):
         """All code for this method lives in this block w.r.t our use-case unless
         u know what ur doing, this code-block is executed when
         `ee stack start --sample` is issued"""
-        _sample_start()
+        _sample_start(ee)
 
     # Additional if-blocks like the above if-block should be added in case you are
     # - adding more than one argument using a single plugin
     # eg) we add --goaccess argument
             """
             if ee.pargs.goaccess:
-                _goaccess_start()
+                _goaccess_start(ee)
             """
 
 
@@ -165,14 +165,14 @@ def sample_stop_hookfunc(ee):
         """All code for this method lives in this block w.r.t our use-case unless
         u know what ur doing, this code-block is executed when
         `ee stack stop --sample` is issued"""
-        _sample_stop()
+        _sample_stop(ee)
 
     # Additional if-blocks like the above if-block should be added in case you are
     # - adding more than one argument using a single plugin
     # eg) we add --goaccess argument
             """
             if ee.pargs.goaccess:
-                _goaccess_stop()
+                _goaccess_stop(ee)
             """
 
 
@@ -182,14 +182,14 @@ def sample_reload_hookfunc(ee):
         """All code for this method lives in this block w.r.t our use-case unless
         u know what ur doing, this code-block is executed when
         `ee stack reload --sample` is issued"""
-        _sample_reload()
+        _sample_reload(ee)
 
     # Additional if-blocks like the above if-block should be added in case you are
     # - adding more than one argument using a single plugin
     # eg) we add --goaccess argument
             """
             if ee.pargs.goaccess:
-                _goaccess_reload()
+                _goaccess_reload(ee)
             """
 
 
@@ -199,13 +199,13 @@ def sample_restart_hookfunc(ee):
         """All code for this method lives in this block w.r.t our use-case unless
         u know what ur doing, this code-block is executed when
         `ee stack restart --sample` is issued"""
-        _sample_restart()
+        _sample_restart(ee)
     # Additional if-blocks like the above if-block should be added in case you are
     # - adding more than one argument using a single plugin
     # eg) we add --goaccess argument
             """
             if ee.pargs.goaccess:
-                _goaccess_restart()
+                _goaccess_restart(ee)
             """
 
 
@@ -215,14 +215,14 @@ def sample_upgrade_hookfunc(ee):
         """All code for this method lives in this block w.r.t our use-case unless
         u know what ur doing ,this code-block is executed when
         `ee stack upgrade --sample` is issued"""
-        _sample_upgrade()
+        _sample_upgrade(ee)
 
     # Additional if-blocks like the above if-block should be added in case you are
     # - adding more than one argument using a single plugin
     # eg) we add --goaccess argument
             """
             if ee.pargs.goaccess:
-                _goaccess_upgrade()
+                _goaccess_upgrade(ee)
             """
 
 
